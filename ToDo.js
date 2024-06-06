5eaf719b980834b3dcc58115df59e9afcad5088f
Manish-Yadav77 <manishkyadav969@gmail.com>
2024-06-06 14:37:30 +0530

    new commit

--- ToDo.js
+++ ToDo.js
@@ -2,7 +2,8 @@ let inp = document.getElementById('inp')
 let text=document.querySelector(".text")
 function Add()
 {  if(inputs.value=="")
-        {     alert("please enter task")
+        {     
+            alert("please enter task")
         }
     else{
         let newele=document.createElement("ul")
